1c9088f371d983b9893494d28340db49920e3521
Lijie Huang <huanglijie@outlook.com>
2018-07-10 21:45:18 +0800

    update

--- featviz/one_step_featviz.py
+++ featviz/one_step_featviz.py
@@ -17,7 +17,7 @@ from salexnet import AlexNet
 def savearray(a, dir_name, filename):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name, mode=0755)
-    print a.max(), a.min()
+    a[a>0.8] = 1
     a = np.uint8(np.clip(a, 0, 1)*255)
     plt.imshow(a)
     plt.savefig(os.path.join(dir_name, filename+'.png'))
