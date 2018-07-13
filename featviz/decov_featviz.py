9457dc09d29f96957b5ebb248d1a29c07c4de726
Lijie Huang <huanglijie@outlook.com>
2018-07-13 22:41:12 +0800

    update

--- featviz/decov_featviz.py
+++ featviz/decov_featviz.py
@@ -3,7 +3,7 @@
 
 import os
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 import numpy as np
 from scipy.misc import imresize
 from PIL import Image
