84cacf623f5c58d2c1e46767b495a578a96b77e6
Lijie Huang <huanglijie@outlook.com>
2018-07-13 22:45:34 +0800

    update

--- featviz/decov_featviz.py
+++ featviz/decov_featviz.py
@@ -9,7 +9,6 @@ from scipy.misc import imresize
 from PIL import Image
 import tensorflow as tf
 
-
 import sys
 sys.path.append('../')
 from salexnet import AlexNet
@@ -29,13 +28,9 @@ test_data_list = [line.strip().split()[0] for line in test_data_list]
 # mean of imagenet dataset in BGR
 imagenet_mean = np.array([104., 117., 124.], dtype=np.float32)
 # model config
-t_preprocessed = tf.placeholder(tf.float32, shape=(1, 227, 227, 3))
-#t_input = tf.placeholder(tf.float32, shape=(380, 330, 3))
-#t_aligned = tf.image.resize_image_with_crop_or_pad(t_input, 380, 380)
-#t_resized = tf.image.resize_images(t_aligned, [227, 227])
-#t_preprocessed = tf.expand_dims(t_resized-imagenet_mean, 0)
+t_input = tf.placeholder(tf.float32, shape=(1, 227, 227, 3))
 keep_prob = tf.placeholder(tf.float32)
-net = AlexNet(t_preprocessed, keep_prob, 4, [])
+net = AlexNet(t_input, keep_prob, 4, [])
  
 # create tensorflow session
 saver = tf.train.Saver()
@@ -45,7 +40,6 @@ graph = sess.graph
 
 # get Conv2D layer name
 layers = [op.name for op in graph.get_operations() if op.type=='Conv2D']
-layers = layers[-2:]
 feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) 
                 for name in layers]
 print('Number of layers', len(layers))
@@ -60,14 +54,12 @@ img0 = np.expand_dims(imresize(img0, (227, 227))-imagenet_mean, 0)
 
 for layer in layers:
     channel_num = int(graph.get_tensor_by_name(layer+':0').get_shape()[-1])
-    for channel in range(channel_num):
-        print 'Viz feature of Layer %s, Channel %s'%(layer, channel)
+    print 'Viz feature of Layer %s, %s Channels'%(layer, channel_num)
 
-        is_success = deconv_visualization(sess_graph_path=sess,
-                                          value_feed_dict={t_preprocessed: img0,
-                                          #value_feed_dict={t_input: img0,
-                                                           keep_prob: 1.},
-                                          layers=layer,
-                                          path_logdir='EmoNetLog',
-                                          path_outdir='EmoNetOut')
+    is_success = deconv_visualization(sess_graph_path=sess,
+                                      value_feed_dict={t_input: img0,
+                                                       keep_prob: 1.},
+                                      layers=layer,
+                                      path_logdir='EmoNetLog',
+                                      path_outdir='EmoNetOut')
 
