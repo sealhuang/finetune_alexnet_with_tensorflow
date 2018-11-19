503916ead0921875f80af528769934dc12bdb447
Lijie Huang <huanglijie@outlook.com>
2018-11-19 20:29:58 +0800

    update

--- finetune_affectnet_on_bazhong.py
+++ finetune_affectnet_on_bazhong.py
@@ -5,7 +5,7 @@
 
 import os
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import numpy as np
 import tensorflow as tf
 
