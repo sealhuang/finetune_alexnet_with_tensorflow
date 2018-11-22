28a7c365a0b64e16b2ce8202d11f2ba5693ef14d
Lijie Huang <huanglijie@outlook.com>
2018-11-22 16:58:15 +0800

    update

--- finetune_affectnet_on_bazhong_cls.py
+++ finetune_affectnet_on_bazhong_cls.py
@@ -25,9 +25,9 @@ val_file = os.path.join(current_dir, 'bazhong', 'val_list.csv')
 #test_file = os.path.join(current_dir, 'genius', 'test_list.csv')
 
 # Learning params
-learning_rate = 0.000005
-num_epochs = 100
-batch_size = 24
+learning_rate = 0.000001
+num_epochs = 40
+batch_size = 21
 
 # Network params
 dropout_rate = 0.5
